e59913ee2eafbea90e1b9390199dd972f288c1df
the514 <the514xx@gmail.com>
2017-08-25 23:44:22 +0800

    package worstui

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = require('./lib/App');
+exports.default = require('./lib/App');
+
+exports.Global = require('./lib/Global');
+exports.List = require('./lib/List');
+exports.Table = require('./lib/Table');
+
+exports.Table = require('./lib/Login/Login');
+
+exports.AddModel = require('./lib/TplBox/AddModel');
+exports.ShowModel = require('./lib/TplBox/ShowModel');
+exports.EditModel = require('./lib/TplBox/EditModel');
+
+exports.EditTag = require('./lib/TplBox/EditTag');
+
+exports.FormatDate = require('./lib/TplBox/FormatDate');
+
+exports.CkBox = require('./lib/TplBox/CkBox');
+exports.Ckeditor = require('./lib/TplBox/Ckeditor');
+exports.CkeditorImg = require('./lib/TplBox/CkeditorImg');
+
+exports.GetColumnConfig = require('./lib/TplBox/GetColumnConfig');
+exports.GetColumnTpl = require('./lib/TplBox/GetColumnTpl');
+exports.GetFilterTpl = require('./lib/TplBox/GetFilterTpl');
+
+exports.ImgModal = require('./lib/TplBox/ImgModal');
+exports.ImgUpload = require('./lib/TplBox/ImgUpload');
+
+exports.SelectBox = require('./lib/TplBox/SelectBox');
+exports.WorkflowState = require('./lib/TplBox/WorkflowState');
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['es2015', 'react', 'stage-0']
           }
         }
       }
